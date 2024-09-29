3973e5685af796f94e6fbd53eb451f12ae7aefc5
MaximeVE <m.vettorato48@gmail.com>
2024-09-29 12:54:30 +0200

    Fix id related bugs Add function to change TextSize Add function to change color Improve screen display

--- app/src/main/java/fr/isep/hello/MainActivity.java
+++ app/src/main/java/fr/isep/hello/MainActivity.java
@@ -1,6 +1,5 @@
 package fr.isep.hello;
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +9,81 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import fr.isep.hello.R;
-
 public class MainActivity extends AppCompatActivity {
 
-    Button AddButton;
-    EditText EditText;
+    //Button
+    Button EditButton;
+    Button AddFontButton;
+    Button MinusFontButton;
+    Button ColorButton;
+
+    int currentColor = -1; //Init color at first item of the list
+    /** Color element for android are int defined by ARGB code 'Alpha, Red, Green, Blue' */
+    int[] colorList = {
+        android.graphics.Color.RED,        //   0xFFFF0000
+        android.graphics.Color.GREEN,      //   0xFF00FF00
+        android.graphics.Color.BLUE,       //   0xFF0000FF
+        android.graphics.Color.YELLOW,     //   0xFFFFFF00
+        android.graphics.Color.CYAN,       //   0xFF00FFFF
+        android.graphics.Color.MAGENTA,    //   0xFFFF00FF
+        android.graphics.Color.GRAY,       //   0xFF808080
+        android.graphics.Color.BLACK,      //   0xFF000000
+        android.graphics.Color.WHITE       //   0xFFFFFFFF
+    };
+
+    //Text
+    EditText EditTextField;
     TextView TextField;
 
-    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        AddButton = findViewById(R.id.edit_text_button);
-        EditText = findViewById(R.id.edit_text);
-        TextField = findViewById(R.id.edit_text_field);
+        EditButton = findViewById(R.id.edit_text_button);
+        AddFontButton = findViewById(R.id.increase_font_button);
+        MinusFontButton = findViewById(R.id.decrease_font_button);
+        ColorButton = findViewById(R.id.change_color_button);
 
-        AddButton.setOnClickListener(new View.OnClickListener() {
+
+        EditTextField = findViewById(R.id.edit_text_field);
+        TextField = findViewById(R.id.edit_text);
+
+        EditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextField.setText(EditText.getText());
+                TextField.setText(EditTextField.getText());
 
+            }
+        });
+
+        AddFontButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextField.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, TextField.getTextSize() + 5); // Value "android.util.TypedValue.COMPLEX_UNIT_PX"
+                                                                                                                 //  allow a definition of size in pixels (PX)
+            }
+        });
+
+        MinusFontButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextField.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, TextField.getTextSize() - 5);
+
+            }
+        });
+
+        ColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newColor;
+                if (currentColor != colorList.length - 1) {
+                    currentColor += 1;
+                } else {
+                    currentColor = 0;
+                }
+                newColor = colorList[currentColor];
+                TextField.setTextColor(newColor);
             }
         });
 
