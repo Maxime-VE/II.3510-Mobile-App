0bcdf6edf9c2d5b6227a070babebf06d37549f0d
miapanc <giuliettapancari@gmail.com>
2024-10-22 19:09:32 +0200

    c'est giu qui met le texte barré

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
 
 dependencies {
@@ -37,5 +38,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("io.realm:realm-android-library:10.15.1") // version récente de Realm
+
 
 }
